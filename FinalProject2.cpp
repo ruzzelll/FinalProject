0994701217fadf05a8a4ef442bb4963088cc188d
Noah Macaldo <202110140@fit.edu.ph>
2021-10-22 12:24:22 +0800

    Added space after common diseases and asterisks in diseased names

--- FinalProject2.cpp
+++ FinalProject2.cpp
@@ -18,7 +18,10 @@ class Diseases {
             d001.desc = "An abdominal aortic aneurysm (AAA) is a swelling (aneurysm) of the aorta \- the main blood vessel that leads- \naway from the heart, down through the abdomen to the rest of the body.";
             d001.treatment = "Surgery: \tReplacing the weakened section of the blood vessel with a piece of synthetic tubing.\nMedications: \tBeta blockers, Angiotensin II receptor blockers, Statins";
             system("cls");
-            cout << d001.type << "\n\n" << d001.desc << "\n\n" << d001.treatment;
+            cout << "****************************************************************************************************\n";
+            cout << d001.type;
+            cout << "\n****************************************************************************************************";
+            cout << "\n\n" << d001.desc << "\n\n" << d001.treatment;
         }
         void d002() {
             Diseases d002;
@@ -26,7 +29,10 @@ class Diseases {
             d002.desc = "Acne is a common skin condition that affects most people at some point. It causes spots, oily-\nskin and sometimes skin that's hot or painful to touch.\n";
             d002.treatment = "Medications: \tTopical retinoids, topical antibiotics, azelaic acid, antibiotic tablets, and isotretinoin tablets";
             system("cls");
-            cout << d002.type << "\n\n" << d002.desc << "\n" << d002.treatment;
+            cout << "****************************************************************************************************\n";
+            cout << d002.type;
+            cout << "\n****************************************************************************************************";
+            cout << "\n\n" << d002.desc << "\n" << d002.treatment;
         }
         void d003() {
             Diseases d003;
@@ -34,7 +40,10 @@ class Diseases {
             d003.desc = "Acute cholecystitis is swelling (inflammation) of the gallbladder. The main symptom of acute-\ncholecystitis is a sudden sharp pain in the upper right side of your tummy (abdomen) that\nspreads towards your right shoulder.";
             d003.treatment = "Surgery: \tA.) Laparoscopic cholecystectomy - a type of keyhole surgery where the gallbladder\n\t\tis removed using special surgical instruments inserted through a number\n\t\tof small cuts (incisions) in your abdomen.\n\n\t\tB.) Open cholecystectomy - where the gallbladder is removed through a single, \n\t\tlarger incision in your abdomen.\n\t\nMedications: \tIf you have a suspected infection, you will also be given antibiotics.";
             system("cls");
-            cout << d003.type << "\n\n" << d003.desc << "\n\n" << d003.treatment;
+            cout << "****************************************************************************************************\n";
+            cout << d003.type;
+            cout << "\n****************************************************************************************************";
+            cout << "\n\n" << d003.desc << "\n\n" << d003.treatment;
         }
         void d004() {
             Diseases d004;
@@ -42,7 +51,10 @@ class Diseases {
             d004.desc = "Alcohol-related liver disease (ARLD) refers to liver damage caused by excess alcohol intake.\nThere are several stages of severity and a range of associated symptoms including: feeling sick\nweight loss, loss of appetite, confusion or drowsiness, and vomiting blood.";
             d004.treatment = "Treatment: \tThere's currently no specific medical treatment for ARLD. The main treatment\n\t\tis to stop drinking, preferably for the rest of your life. This reduces the risk of\n\t\tfurther damage to your liver and gives it the best chance of recovering.";
             system("cls");
-            cout << d004.type << "\n\n" << d004.desc << "\n\n" << d004.treatment;
+            cout << "****************************************************************************************************\n";
+            cout << d004.type;
+            cout << "\n****************************************************************************************************";
+            cout << "\n\n" << d004.desc << "\n\n" << d004.treatment;
         }
         void d005() {
             Diseases d005;
@@ -50,7 +62,10 @@ class Diseases {
             d005.desc = "Anxiety is a feeling of unease, such as worry or fear, that can be mild or severe. Anxiety is the\nmain symptom of several conditions, including panic disorder, phobias, post-traumatic stress\ndisorder and social anxiety disorder (social phobia).";
             d005.treatment = "Initial Treatment: \tA.) Cognitive behavioural therapy - CBT helps you to understand how your problems,\n\t\t\tthoughts, feelings and behaviour affect each other. It can also help you to\n\t\t\tquestion your negative and anxious thoughts, and do things you would usually avoid\n\t\t\tbecause they make you anxious.\n\n\t\t\tB.) Applied relaxation - an alternative type of psychological treatment that can be\n\t\t\tas effective as CBT in treating GAD. Applied relaxation focuses on relaxing your muscles \n\t\t\tin a particular way during situations that usually cause anxiety.\n\nMedications: \tYou may be prescribed with the following medications: selective serotonin reuptake inhibitors (SSRIs),\n\t\tserotonin and noradrenaline reuptake inhibitors (SNRIs), and pregabalin benzodiazepines.";
             system("cls");
-            cout << d005.type << "\n\n" << d005.desc << "\n\n" << d005.treatment;
+            cout << "****************************************************************************************************\n";
+            cout << d005.type;
+            cout << "\n****************************************************************************************************";
+            cout << "\n\n" << d005.desc << "\n\n" << d005.treatment;
         }
 
 };
@@ -58,7 +73,7 @@ class Diseases {
 void draw() {
     cout << "**************************************************\n";
     cout << "\t\tCommon Diseases A-Z\n";
-    cout << "**************************************************\n";
+    cout << "**************************************************\n\n";
     cout << "1. Abdominal aortic aneurysm\n";
     cout << "2. Acne\n";
     cout << "3. Acute cholecystitis\n";
